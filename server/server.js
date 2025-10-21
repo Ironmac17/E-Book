120a1f16b2539c111fc02d1d82c57e95308c6a34
Ironmac17 <nimish4agrawal@gmail.com>
2025-10-21 11:06:23 +0530

    Auth backend

--- server/server.js
+++ server/server.js
@@ -4,6 +4,8 @@ const cors=require("cors")
 const path=require("path");
 const connectDB=require("./config/db")
 
+const authRoutes=require('./routes/authRoutes');
+
 const app=express();
 
 app.use(
@@ -19,6 +21,9 @@ connectDB();
 app.use(express.json());
 
 app.use("/backend/uploads",express.static(path.join(__dirname,"uploads")));
+
+app.use("/api/auth",authRoutes);
+
 
 const PORT=process.env.PORT || 5000;
 app.listen(PORT,()=> console.log(`Server running on Port ${PORT}`));
