1e6fb84a920592d35ab8130c7f99c0ad26d62356
Ironmac17 <nimish4agrawal@gmail.com>
2025-10-30 18:01:48 +0530

    Dropdown

--- client/src/pages/EditorPage.jsx
+++ client/src/pages/EditorPage.jsx
@@ -1,4 +1,15 @@
 import React from 'react'
+import {useParams,useNavigate} from 'react-router-dom'
+import toast from 'react-hot-toast'
+import {Sparkles,FileDown,Save,Menu,X,Edit,NotebookText,ChevronDown,FileText} from 'lucide-react'
+import {arrayMove} from '@dnd-kit/sortable'
+import axiosInsatance from '../utils/axiosInstance'
+import { API_PATHS } from '../utils/apiPaths'
+import Dropdown,{DropdownItem} from '../components/ui/Dropdown'
+import InputField from '../components/ui/Button'
+import Button from '../components/ui/Button'
+import Modal from '../components/ui/Modal'
+import SelectField from '../components/ui/SelectField'
 
 const EditorPage = () => {
   return (
